ae1cc61727b4f5bbabb4d77393af0dba788ca3ad
Roy Springer <1014183@mborijnland.nl>
2019-06-25 12:40:13 +0200

    Inleveren

---
+++ src/Entity/Kamer.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\KamerRepository")
+ */
+class Kamer
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Soort")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $soort;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Extras")
+     */
+    private $extras;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=0)
+     */
+    private $prijs;
+
+    public function __construct()
+    {
+        $this->extras = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSoort(): ?Soort
+    {
+        return $this->soort;
+    }
+
+    public function setSoort(?Soort $soort): self
+    {
+        $this->soort = $soort;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Extras[]
+     */
+    public function getExtras(): Collection
+    {
+        return $this->extras;
+    }
+
+    public function addExtra(Extras $extra): self
+    {
+        if (!$this->extras->contains($extra)) {
+            $this->extras[] = $extra;
+        }
+
+        return $this;
+    }
+
+    public function removeExtra(Extras $extra): self
+    {
+        if ($this->extras->contains($extra)) {
+            $this->extras->removeElement($extra);
+        }
+
+        return $this;
+    }
+
+    public function getPrijs()
+    {
+        return $this->prijs;
+    }
+
+    public function setPrijs($prijs): self
+    {
+        $this->prijs = $prijs;
+
+        return $this;
+    }
+}
